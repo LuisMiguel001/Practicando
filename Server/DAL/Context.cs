f20c3bd3cb936465886ee46dd0eaebf62c4d910e
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-08 20:08:52 -0400

    Agregando el contexto.

---
+++ Server/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practicando.Shared.Models;
+
+namespace Practicando.Server.DAL
+{
+	public class Context : DbContext
+	{
+		public Context(DbContextOptions<Context> Options)
+			: base(Options) { }
+
+		public DbSet<Ingresos>	Ingresos { get; set; }
+	}
+}
