f718b50938cb217d4bd6d29a4055fde7b3b6d80e
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-09 08:33:02 -0400

    Agregando el detalle.

---
+++ Shared/Models/Detalle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicando.Shared.Models
+{
+	public class Detalle
+	{
+		[Key]
+
+		public string? Emisor { get; set; }
+
+		public string? Mensaje { get; set; }
+	}
+}
